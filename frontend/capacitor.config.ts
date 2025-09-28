4e2b848f44e26e938ca2d379ba977af962bf63cd
CraftApp Automation <no-reply@craftapp.com>
2025-09-28 10:02:21 +0000

    Initial master branch

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.craftapp.app",
-  appName: "craftapp",
-  webDir: "out",
-};
-
-export default config;
